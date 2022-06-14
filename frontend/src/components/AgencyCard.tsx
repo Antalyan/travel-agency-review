0e2c5edd8f78240745d7ac0897d6cf597eb7c4ec
Michael Koudela <xkoudel2@fi.muni.cz>
2022-06-14 16:39:21 +0200

    Fixed card layout

--- frontend/src/components/AgencyCard.tsx
+++ frontend/src/components/AgencyCard.tsx
@@ -6,7 +6,7 @@ import Box from "@mui/material/Box";
 import Profile from "../../images/mock_profile.jpg";
 import DoneOutlineIcon from '@mui/icons-material/DoneOutline';
 import CancelOutlinedIcon from '@mui/icons-material/CancelOutlined';
-import { Rating } from "@mui/material";
+import {Card, CardContent, Container, Rating, Stack} from "@mui/material";
 
 export interface IAgencyOverviewCard {
     id: number,
@@ -18,55 +18,55 @@ export interface IAgencyOverviewCard {
 
 export function AgencyOverviewCard({id, name, insuranceValid, overallScore, totalReviews}: IAgencyOverviewCard) {
     return (
-        <Grid container rowSpacing={1}
-              columnSpacing={{xs: 0}}
-              marginLeft={{md: "auto"}}
-              marginRight={{md: "auto"}}
-              maxWidth={{md: 960}}>
-            <Grid item xs={8}>
-                <Typography sx={{m: 2}}
-                            variant="h3"
-                            align="center"
-                            color="text.primary"
-                            gutterBottom
-                            fontWeight="bold"
-                > {name}
-                </Typography>
-            </Grid>
-            <Grid item xs={2}>
-                <Typography sx={{m: 2}}
-                            variant="body2"
-                            align="right"
-                            color="text.primary"
-                > INSURANCE <br/> {+insuranceValid ? "VALID" : "INVALID"}
-                </Typography>
-            </Grid>
-            <Grid item xs={2}>
-                {insuranceValid ? <DoneOutlineIcon/> : <CancelOutlinedIcon/>}
-            </Grid>
-            <Grid item xs={4} container direction="column" paddingRight={4} justifyContent={"flex-start"}
-                  alignItems={"flex-end"} marginTop={3}>
-                {/*TODO: fix href*/}
-                <Button href={`/doctor/${id}`} variant='contained' color={'primary'}
-                        size={"large"}>DETAIL</Button>
-            </Grid>
-            <Grid item xs={8}>
-                <Rating name="main-rating" value={overallScore} precision={0.5} readOnly
-                        sx={{color: "primary.main"}}/>
-                <Typography sx={{m: 2}}
-                            variant="body2"
-                            align="left"
-                            color="text.primary"
-                >based on {totalReviews} reviews
-                </Typography>
-            </Grid>
-            <Grid item xs={4} container direction="column" paddingRight={4} justifyContent={"flex-start"}
-                  alignItems={"flex-end"} marginTop={3}>
-                {/*TODO: fix href*/}
-                <Button href={`/doctor/${id}`} variant='contained' color={'primary'}
-                        size={"large"}>DETAIL</Button>
-            </Grid>
-        </Grid>
+       <Grid container justifyContent={"center"} maxWidth={{md: 960}} marginLeft={{md: "auto"}} marginRight={{md: "auto"}}>
+           <Grid item xs={11} marginBottom={2}>
+               <Card>
+                   <CardContent>
+                       <Grid container rowSpacing={1}
+                             columnSpacing={{xs: 0}}
+                             justifyContent={"center"}
+                       >
+                           <Grid item xs={8}>
+                               <Typography sx={{m: 2}}
+                                           pl={2}
+                                           variant="h3"
+                                           color="text.primary"
+                                           gutterBottom
+                                           fontWeight="bold"
+                               > {name}
+                               </Typography>
+                           </Grid>
+                           <Grid item xs={4} container direction="column" paddingRight={4}
+                                 alignItems={"flex-end"} marginTop={3}>
+                                  <Stack direction={"row"} spacing={2}>
+                                      <Typography variant="body2"
+                                                  align="right"
+                                                  color={insuranceValid ? "green" : "red"}
+                                      > INSURANCE <br/> {+insuranceValid ? "VALID" : "INVALID"}
+                                      </Typography>
+                                      {insuranceValid ? <DoneOutlineIcon sx={{color: "green"}}/> : <CancelOutlinedIcon sx={{color: "red"}}/>}
+                                  </Stack>
+                           </Grid>
+                           <Grid item xs={8} pl={4}>
+                               <Rating name="main-rating" value={overallScore} precision={0.5} readOnly
+                                       sx={{color: "primary.main"}}/>
+                               <Typography variant="body2"
+                                           align="left"
+                                           color="text.primary"
+                               >based on {totalReviews} reviews
+                               </Typography>
+                           </Grid>
+                           <Grid item xs={4} container direction="column" paddingRight={4}
+                                 alignItems={"flex-end"} marginTop={3}>
+                               {/*TODO: fix href*/}
+                               <Button href={`/doctor/${id}`} variant='contained' color={'primary'}
+                                       size={"large"}>DETAIL</Button>
+                           </Grid>
+                       </Grid>
+                   </CardContent>
+               </Card>
+           </Grid>
+       </Grid>
     )
 }
 
